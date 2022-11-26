fe14171b3e035f113aa711ea4ee0d2eff7d29f18
Aleksandr Grigorev <ag@inbox.ru>
2022-11-27 01:21:40 +0500

    Add app

--- app.py
+++ app.py
@@ -9,14 +9,21 @@ app = flask.Flask(__name__, template_folder='templates')
 @app.route('/', methods=['POST', 'GET'])
 
 @app.route('/index', methods=['POST', 'GET'])
-
 def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
 
     if flask.request.method == 'POST':
-        with open('welding.pkl', 'rb') as F:
+        with open('jupyter\welding.pkl', 'rb') as F:
             loaded_model = pickle.load(F)
 
+        welding_params = float(flask.request.form['IW'])
+        #preds = loaded_model.predict([[welding_params]])
+        preds = welding_params
+
+        return render_template('main.html', result = preds)
+
+if __name__ == '__main__':
+    app.run()
             
             
